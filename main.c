983a3359dd6ab9e8fb99c867f44833732b6d6589
Shivansh Jaiswal <shivansh12kol34@gmail.com>
2026-01-29 22:01:02 +0530

    fix: added main.c for compilation

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "state.h"
+#include "parser.h"
+
+int main(int argc, char *argv[]) {
+    state_ state;
+    init_chip(&state);
+
+    return 0;
+}
